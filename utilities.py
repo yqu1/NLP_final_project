c1d349ce5fa1ff0295d82d579a39d4217f381156
quyaoxian <quyaoxian@gmail.com>
2017-11-23 01:51:45 -0500

    update utilities and code for calculating document similarity

--- utilities.py
+++ utilities.py
@@ -7,3 +7,17 @@ def get_text_from_xml(filename):
 		if elem.tag == 'ORIGINAL_TEXT':
 			text.append(elem.text)
 	return text
+
+def get_text_from_bio(filename):
+	data = list()
+	with open(filename) as f:
+		lines = f.readlines()
+		temp = []
+		for l in lines:
+			l = l.split()
+			if len(l) > 1:
+				temp.append(l[0])
+			else:
+				data.append(temp)
+				temp = []
+	return data
