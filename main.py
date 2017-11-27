6dd32ab32e54c0815b183b8415c838d1e719db86
quyaoxian <quyaoxian@gmail.com>
2017-11-27 12:26:04 -0500

    ddd

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ class Similarity_Model(object):
 	def __init__(self):
 		self.source_doc = ''
 		self.target_docs = []
-		self.ds = DocSim(KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin.gz', binary=True))
+		self.ds = DocSim(KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True))
 	
-	def get_similarity(sd):
-		sd = self.vectorize(sd)
+	def get_similarity(self, sd):
+		sd = self.ds.vectorize(sd)
 		result = {}
 		for fd in os.listdir('data_transform'):
 			with open("data_transform/" + fd) as f:
-				result[fd] = self.ds._cosine_sim(sd, self.vectorize(f.read()))
+				result[fd] = self.ds._cosine_sim(sd, self.ds.vectorize(f.read()))
 		return result
