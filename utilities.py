f1d82313e4f6c60e829f5c5c41b1dcf27215ce7b
quyaoxian <quyaoxian@gmail.com>
2017-11-27 23:21:40 -0500

    RNN

--- utilities.py
+++ utilities.py
@@ -1,4 +1,9 @@
 import xml.etree.ElementTree as ET
+import re
+import os
+
+def clean(s):
+    return re.sub('[^a-zA-Z]+', ' ', s)
 
 def get_text_from_xml(filename):
 	tree = ET.parse(filename)
@@ -17,8 +22,27 @@ def get_text_from_bio(filename, outname):
 			l = l.split()
 			if len(l) > 1:
 				temp.append(l[0])
-				f_out.write(" ".join(temp) + "\n")
 			else:
+				f_out.write(" ".join(temp) + "\n")
 				temp = []
 	f_out.close()
 
+# convert latin document to list of sentences
+def docToSentences(filename):
+	sentences = []
+	with open(filename) as f:
+		lines = f.readlines()
+		for l in lines:
+			sentences.append(clean(l.strip('\n')))
+		sentences = [sent.lower() for sent in sentences]
+	return sentences
+
+def createDataLabel():
+	labels = []
+	docs = []
+	for fd in os.listdir('data_latin'):
+		label = fd.split('.')[0]
+		doc = docToSentences('data_latin/' + fd)
+		labels.append(label)
+		docs.append(doc)
+	return docs, labels
