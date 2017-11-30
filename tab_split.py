93a54602f7c00c6eaf4ac65e9963f702653b8780
Nicholas David Smith <nsmith9@crcfe01.crc.nd.edu>
2017-11-30 00:16:36 -0500

    tab splitting/preprocessing

---
+++ tab_split.py
@@ -0,0 +1,27 @@
+import csv
+import os
+from utilities import *
+import json
+import re
+import string
+
+doc_tags = {}
+directory = '/afs/crc.nd.edu/user/n/nsmith9/NLP_final_project/sftype/orm/LDC2017E58_LORELEI_IL6_Incident_Language_References_for_Year_2_Eval_Unsequestered_V1.0/setE/data/annotation/situation_frame/issues'
+for filename in os.listdir(directory):
+    with open(directory + '/' +  filename) as tsv:
+        n = 0
+        for line in csv.reader(tsv, dialect="excel-tab"):
+            if n == 1:
+                doc_tags[line[1] + '.ltf.xml'] = line[4]
+            n+=1
+docroot = '/afs/crc.nd.edu/user/n/nsmith9/NLP_final_project/sftype/orm/LDC2017E58_LORELEI_IL6_Incident_Language_References_for_Year_2_Eval_Unsequestered_V1.0/setE/data/translation/il6/ltf'
+text_tags = {}
+regex = re.compile('[^a-zA-Z0-9 ]')
+for doc in doc_tags:
+    temp = get_text_from_xml(docroot + '/' + doc)
+    temp = ' '.join(temp).replace('_','')
+    if len(temp.strip()) > 0:
+        temp = regex.sub('',temp)
+        text_tags[temp] = doc_tags[doc]
+text_tags = json.dumps(text_tags)
+print(text_tags)
